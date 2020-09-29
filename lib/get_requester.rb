f3fe53f540b1d42192dfd0df983e1634df5d1ba1
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-29 14:30:55 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+ 
+class GetRequester
+  
+  def initialize(url)
+  @url = URL
+  end
+  
+  def get_response_body
+    uri = URI(@url)
+    Net::HTTP.get(uri)
+  end
+  
+  def parse_json
+    
+    
+   
+      
+ 
+  end
+end
+ 
